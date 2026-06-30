ca32d05e82fc5fa6cc3cb878cd5cf59b216a1611
gnil000 <meowmeow666kot@gmail.com>
2026-06-30 19:18:31 +0300

    removed fmt.Println

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import "fmt"
 func main() {
 	rabbit := &Rabbit{}
 
-	_ = rabbit.Poop(false)
-
-	fmt.Println("very long row. this row longer than 140 characters and go vet must return error for this. After need remove or cut this row. Yes complete.")
+	_ = rabbit.Poop(true)
 }
 
 type Rabbit struct {
